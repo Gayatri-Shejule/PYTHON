6cf233522fa0f680dd9b937d2c4fab6c6a27bc55
Gayatri Shejule <gayatrishejule454@gmail.com>
2025-10-04 05:20:58 +0530

    python3.py

---
+++ PYTHON/python3.py
@@ -0,0 +1,22 @@
+#variables and data types
+#variable names are case sensitive
+
+x = 5
+y = "Hello, World!"
+#both are correct and valid
+#y='Hello, World!'
+z=4.5
+#not overwriting the value of z
+Z=7.5
+a=True
+
+print(x)
+print(y)
+print(z)
+print(Z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(Z))
+print(type(a))
