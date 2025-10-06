30a5e93fc7c373622b3440682c5dab8215b4c0fa
Gayatri Shejule <gayatrishejule454@gmail.com>
2025-10-06 09:04:30 +0530

    python5.py

---
+++ PYTHON/python5.py
@@ -0,0 +1,8 @@
+# Different naming conventions in Python
+myVariableName = "Gayatri Shejule"#camel case
+MyVariableName = "Gayatri Shejule"#pascal case
+my_variable_name = "Gayatri Shejule"#snake case
+
+print(myVariableName)
+print(MyVariableName)
+print(my_variable_name)
